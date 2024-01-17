e4b870c597664f67840b98f0dcf12f0b42253c64
mrshaikat <mrshaikat0.cse@gmail.com>
2024-01-17 22:32:26 +0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./tasks/TaskBoard";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <TaskBoard />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
